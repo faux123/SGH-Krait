706f48735a11787b777f1079cc7b8e7e5b7699a7
faux123 <reioux@gmail.com>
2012-12-18 07:37:57 -0800

    intellidemand: performance tune up
    Signed-off-by: faux123 <reioux@gmail.com>

--- drivers/cpufreq/cpufreq_intellidemand.c
+++ drivers/cpufreq/cpufreq_intellidemand.c
@@ -1146,10 +1146,10 @@ unsigned long get_lmf_inactive_load(void)
 #define NR_FSHIFT	1
 static unsigned int nr_run_thresholds[] = {
 /* 	1,  2 - on-line cpus target */
-	4,  UINT_MAX /* avg run threads * 2 (e.g., 9 = 2.25 threads) */
+	3,  UINT_MAX /* avg run threads * 2 (e.g., 9 = 2.25 threads) */
 	};
 
-static unsigned int nr_run_hysteresis = 2;  /* 0.5 thread */
+static unsigned int nr_run_hysteresis = 8;  /* 0.5 thread */
 static unsigned int nr_run_last;
 
 static unsigned int calculate_thread_stats (void)
