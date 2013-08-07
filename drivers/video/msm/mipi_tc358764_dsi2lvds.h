be4cddbf261bd3830958bf0e94a6fab1bc9caf5b
Paul Reioux <reioux@gmail.com>
2013-08-07 17:12:04 -0500

    initial android patches for d2vzw (SCH-535) from Samsung (MF1)
    Signed-off-by: Paul Reioux <reioux@gmail.com>

---
+++ drivers/video/msm/mipi_tc358764_dsi2lvds.h
@@ -0,0 +1,26 @@
+/* Copyright (c) 2011, Code Aurora Forum. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef MIPI_TC358764_DSI2LVDS_H
+#define MIPI_TC358764_DSI2LVDS_H
+
+struct dsi2lvds_panel_data {
+	const char panel_name[30];
+};
+struct dsi2lvds_driver_data {
+	struct dsi2lvds_panel_data *dpd;
+};
+
+int mipi_tc358764_dsi2lvds_register(struct msm_panel_info *pinfo,
+	u32 channel_id, u32 panel_id, struct dsi2lvds_panel_data *dpd);
+#endif  /* MIPI_TC358764_DSI2LVDS_H */
